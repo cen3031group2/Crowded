5e66925c12a8e04799a65704704630dfa1b9a6a0
Isaiah <itbonds@ufl.edu>
2018-10-24 09:42:47 -0400

    Starting to creating out the backend.

---
+++ server/models/userModel.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    userId:  Number,
+    username: String,
+    password: String,
+    genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}],
+    email: String,
+    isEmployee: Boolean,
+    employee_company: [{type: Schema.Types.ObjectId, ref: 'Company'}]
+});
+
+var User = mongoose.model("User", userSchema);
