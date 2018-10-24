53fc5c0f41c97b3efb34d1311d2bf827b6b6a2e1
Samantha Garcia <sgarcia22@ufl.edu>
2018-10-24 15:42:10 -0400

    Added Node Modules

--- client/js/factories/moviesFactory.js
+++ client/js/factories/moviesFactory.js
@@ -1,19 +1,19 @@
-angular.module('crowdy').factory('Movies', function() {
-  var movies = {
+angular.module('movies').factory('Movies', function() {
+  var moviesEntries = {
     entries : [
       {
         "id" : 0,
-        "name" : 'Jaws',
-        "genre" : 'Horror',
+        "name" : "Jaws",
+        "genre" : "Horror",
         "ratings" : 1
-      }
+      },
       {
         "id" : 1,
-        "name" : 'Crazy Rich Asians',
-        "genre" : 'Comedy',
+        "name" : "Crazy Rich Asians",
+        "genre" : "Comedy",
         "ratings" : 5
       }
     ]
-    return movies.entries;
   };
-};
+  return moviesEntries.entries;
+});
