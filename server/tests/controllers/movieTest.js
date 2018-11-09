d0dc5017b39be8af44d168ad3b76b389035f5576
ibonds <itbonds@ufl.edu>
2018-11-08 22:41:58 -0500

    Pushing some work. Work still needs to be done on fixing the async calls currently

---
+++ server/tests/controllers/movieTest.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+var movie = require('../../controllers/movieController');
+var mongoose = require('mongoose'), 
+ config = require('../../config/config');
+
+
+describe('Movies', function(){
+    describe('getAllMoviesFromTheater', function(){
+        it('should return movies', function(){
+            var req = {};
+            req.body = {
+                id: 42490
+            };
+            var res = {};
+            res.json = function(movies){
+                console.log(movies);
+            };
+            movie.getAllMoviesFromTheater(req,res);
+        })
+    })
+});
