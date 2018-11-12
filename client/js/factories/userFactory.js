ae657fb4262e6f37d851f6b22267c81233c6ee94
Samantha Garcia <sgarcia22@ufl.edu>
2018-11-12 12:26:50 -0500

    Fixing Controller Logic

--- client/js/factories/userFactory.js
+++ client/js/factories/userFactory.js
@@ -1,10 +1,41 @@
-angular.module('crowdy').factory('Users', function() {
-  var theaters = {
-    entries : [
-      {
-        //Fill entries with users
-      }
-    ]
-    return theaters.entries;
-  };
-};
+//Isaiah's code
+angular.module('crowdy', []).factory('User', function($http) {
+    var methods = {
+        getGenres: function(user){
+            return $http.post('../api/genre/get', user);
+        },
+        setGenres: function(user, genres){
+            const body = {
+                username: user.username,
+                genres: genres
+            };
+            return $http.post('../api/genre/set', body);
+        },
+
+        setHistory: function(user, history){
+            const body = {
+                username: user.username,
+                history: history
+            };
+            return $http.post('../api/user/history/set', body);
+        },
+        getHistory: function(user, history){
+            return $http.post('../api/user/history/get', user);
+        },
+
+        checkPassword: function(user){
+            return $http.post('http://localhost:8080/api/user/password/check', user);
+        },
+        setPassword: function(user){
+            return $http.post('http://localhost:8080/api/user/password/set', user);
+        },
+        getCompany: function(user){
+            return $http.post('../api/user/company/get')
+        },
+        createUser: function(user){
+            return $http.post('http://localhost:8080/api/user/create', user);
+        }
+    };
+
+    return methods;
+  });
