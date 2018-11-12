3b50f44cca372ac561feb3871d816a418084da68
Isaiah <itbonds@ufl.edu>
2018-11-12 13:29:01 -0500

    Update some backend code, in addition to writing out some basic controllers.

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,6 @@
 // this is Crowdy's app.js file
 // 10/11/18
-
+const website = 'http://localhost:8080';
 var app = angular.module('directoryApp', []);
 
 app.run(function ($rootScope) {
@@ -9,89 +9,105 @@ app.run(function ($rootScope) {
     });
 });
 
-app.controller('MovieController', function ($scope) {
+app.controller('MovieController', ['$scope', '$http', function ($scope, $http) {
 
     $scope.movies = undefined;
     $scope.codec = undefined;
     $scope.verify = "hello"
-  
-    /* Get all the listings, then bind it to the scope */
-    Movies.getAll().then(function(response) {
-      $scope.movies = response.data;
-    }, function(error) {
-      console.log('Unable to retrieve listings:', error);
-    });
 
+    const movieFormat = {
+      id: 0,
+      title: '',
+      theater: 0,
+      poster_image_thumbnail: '',
+      rating: {
+        imbd: 0,
+        tmbd: 0
+      },
+      genres = [''],
+      showtimes = [{
+        hour: 0,
+        minute: 0
+      }],
+      crowdy: 0,
+    }
     $scope.getMoviesFromTheater = function (theater_id){
-        if (theater_id == null) {
+        if (theater_id == null || !theater_id) {
           theater_id = Scopes.get('TheaterController').currentTheaterId;
         }
-        $scope.movies = Movies.getAllMoviesFromTheater(theater_id)
-    };
-
-    $scope.detailedInfo = function (index) {
-      var movie = $scope.listings[index];
-      //TODO
+        $scope.movies = $http.get(website + '/api/movie/getAllMoviesFromTheater/' + theater_id);
     };
 
     //Check if the input and code or name of the building matches
+    // Isaiah: I don't understand what this is used for?
     $scope.valid = function (json) {
            if ($scope.codec == undefined) return true;
            return (json.name.toLowerCase().startsWith($scope.codec.toLowerCase()) ||
                    json.genre.toLowerCase().startsWith($scope.codec.toLowerCase()));
     };
+}]);
 
-    $scope.addMovies = function() {
-      try {
-        var newMovie =
-          {
-          "id" : $scope.newMovie.id,
-          "name" : $scope.newMovie.name,
-          "genre" : $scope.newMovie.genre,
-          "ratings" : $scope.newMovie.ratings
-          };
-        $scope.movies.push(newMovie);
-        Listings.create(newMovie);
-      }
-      catch (err) {
-        console.log (err);
-      }
-    };
+app.controller('TheaterController', ['$scope', '$http', 'Scopes', function ($scope, $http, Scopes) {
+  $scope.theaters = undefined;
+  const theaterFormat = {
+    id: 0,
+    crowdy: {
+      id: '',
+      public: 0,
+      employee: 0
+    },
+    name: '',
+    website: '',
+    telephone: '',
+    location: {}
+  }
+  $scope.getAllTheaters = function() {
+    $scope.theaters = $http.get(website + '/api/theater/getAllTheaters');
+  }
 
-    $scope.deleteMovie = function(index) {
-       try {
-           var id = $scope.listings[index]._id;
-           $scope.listings.splice(index, 1);
-           Listings.delete(id);
-           $route.reload();
-       }
-       catch (err) {
-         console.log(err);
-       }
-    };
+  $scope.getTheater = function(theater_id){
+    $scope.currentTheater = $http.get(website + '/api/theater/getTheaters/' + theater_id);
+  }
 
-    $scope.showDetails = function(index) {
-      $scope.detailedInfo = $scope.listings[index];
-    };
-});
-
-app.controller('TheaterController', function ($scope, Scopes) {
-
+  // Isaiah: ?
     Scopes.store('TheaterController', $scope);
     $scope.currentTheaterId = 42490;
 
-});
+}]);
 
-app.controller('UserController', function ($scope) {
-    /* Get all the listings, then bind it to the scope */
-    $scope.verify = "hello";
-    $scope.inputEmail = "";
-    $scope.inputPassword = "";
+app.controller('UserController', ['$scope', '$http', function($scope, $http){
+  // returns an array with user's listed genres
+  $scope.getUserGenres = function(username){
+    $http.get(website + '/api/user/genre/' + username);
+  }
 
-    //Check if the input and code or name of the building matches
-    $scope.valid = function () {
-      if($scope.inputEmail == "user@test.com") {
-        location.replace("./crowdy.hmtl");
-      }
-    };
-});
+  // returns user by that name or null if none, does not have password attached
+  const userFormat = {
+    username: '',
+    password: '',
+    genre: [''],
+    email: '',
+    employee_company: ''
+  }
+  $scope.getUser = function(username){
+    $http.get(website + '/api/user/' + username); // returns user object, or null if there is no user
+  }
+
+  $scope.checkPassword = function(userToCheck){
+    // expected format for user to check
+    // {username: '', password: ''};
+    
+    $http.post(website + '/api/user/password/check', userToCheck);// returns true if valid, false otherwise
+  }
+
+  $scope.createUser = function(userToCreate){
+    // expect user to have {username: '', password: ''}
+    var msg = $http.post(website + '/api/user/create', userToCreate); // returns msg object, which will have a msg if failed
+    // msg = {created: boolean, msg: ''}
+  }
+
+  $scope.saveUser = function(userToSave){
+    // expect user to have {username: '', password: ''}
+    $http.post(website + '/api/user/save', userToSave); // saves updates to user.
+  }
+}]);
